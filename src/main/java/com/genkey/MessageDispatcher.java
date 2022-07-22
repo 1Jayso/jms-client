70ab2cc3263ee743753000635dbe7204c930f7cd
hyboforo <hyboforo@gmail.com>
2022-07-22 10:43:22 +0000

    restructured the MessageDispatcher class

--- src/main/java/com/genkey/MessageDispatcher.java
+++ src/main/java/com/genkey/MessageDispatcher.java
@@ -23,8 +23,27 @@ import javax.naming.NamingException;
  */
 public class MessageDispatcher {
 
-   public static void sendHornetqMessage(String host, String port) {
-        Session session = null;
+    public static void sendMessage(ConnectionFactory connectionFactory, Context context) {
+        try {
+            Connection connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = (Queue) context.lookup("/queue/clientResponseQueue");
+            MessageProducer sender = session.createProducer(queue);
+            ObjectMessage jmsMessage = session.createObjectMessage();
+            jmsMessage.setObject("Hi there");
+            sender.send(jmsMessage);
+            System.out.println("Sent message: " + jmsMessage.toString());
+        } catch (JMSException ex) {
+            System.out.println("Failure sending JMS message: " + ex.getLocalizedMessage());
+            throw new RuntimeException(ex);
+        } catch (NamingException ex) {
+            System.out.println("Failure resolving JNDI resources: " + ex.getLocalizedMessage());
+            throw new RuntimeException(ex);
+
+        }
+    }
+
+    public static void sendHornetqMessage(String host, String port) {
         try {
             Properties prop = new Properties();
             prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
@@ -36,32 +55,35 @@ public class MessageDispatcher {
             Context context = new InitialContext(prop);
             System.out.println("Context is " + context);
             ConnectionFactory cf = (ConnectionFactory) context.lookup("jms/RemoteConnectionFactory");
-            Connection connection = cf.createConnection();
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = (Queue) context.lookup("/queue/clientResponseQueue");
-            MessageProducer sender = session.createProducer(queue);
-            ObjectMessage jmsMessage = session.createObjectMessage();
-            jmsMessage.setObject("Hi there");
-            sender.send(jmsMessage);
-            System.out.println("Sent message: " + jmsMessage.toString());
+            sendMessage(cf, context);
         } catch (NamingException ex) {
             System.out.println("Failure resolving JNDI resources: " + ex.getLocalizedMessage());
             throw new RuntimeException(ex);
-
-        } catch (JMSException ex) {
+        } catch (Exception ex) {
             System.out.println("Failure sending JMS message: " + ex.getLocalizedMessage());
             throw new RuntimeException(ex);
         }
 
     }
-    
-    public static void sendArtemisMessage(){
-        System.out.println("Artemis");
+
+    public static void sendArtemisMessage(String host, String port) {
+        try {
+            Properties prop = new Properties();
+            prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory");
+            prop.put(Context.PROVIDER_URL, "tcp://" + host + ":" + port);
+            prop.put(Context.SECURITY_PRINCIPAL, "artemis");
+            prop.put(Context.SECURITY_CREDENTIALS, "artemisadmin");
+            Context context = new InitialContext(prop);
+            System.out.println("Context is " + context);
+            ConnectionFactory cf = (ConnectionFactory) context.lookup("ConnectionFactory");
+            sendMessage(cf, context);
+        } catch (NamingException ex) {
+            System.out.println("Failure resolving JNDI resources: " + ex.getLocalizedMessage());
+            throw new RuntimeException(ex);
+        } catch (Exception ex) {
+            System.out.println("Failure sending JMS message: " + ex.getLocalizedMessage());
+            throw new RuntimeException(ex);
+        }
     }
-    
-    public static void sendActiveMQMessage(){
-        System.out.println("ActiveMQ");
-    }
-    
 
 }
